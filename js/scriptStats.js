8eef1333310599e10856850430f3d4103d5c5ef3
Brian <38541405+Br20@users.noreply.github.com>
2020-06-06 18:11:15 -0300

    stats responsive

--- js/scriptStats.js
+++ js/scriptStats.js
@@ -4,34 +4,34 @@
 
 let rowsGoles = [
     {
-        nombre: "G. Blasco",
-        equipo: "FreeRonaldinho",
-        goles: 7
+        'nombre': "G. Blasco",
+        'equipo': "FreeRonaldinho",
+        'goles': 7
     },
     {
-        nombre: "O. Martinez",
-        equipo: "CA Ford",
-        goles: 5
+        'nombre': "O. Martinez",
+        'equipo': "CA Ford",
+        'goles': 5
     },
     {
-        nombre: "J. Lopez",
-        equipo: "FreeRonaldinho",
-        goles: 4
+        'nombre': "J. Lopez",
+        'equipo': "FreeRonaldinho",
+        'goles': 4
     },
     {
-        nombre: "L. Quinteros",
-        equipo: "Bohemio F.C.",
-        goles: 4
+        'nombre': "L. Quinteros",
+        'equipo': "Bohemio F.C.",
+        'goles': 4
     },
     {
-        nombre: "G. Usinger",
-        equipo: "Anfield",
-        goles: 3
+        'nombre': "G. Usinger",
+        'equipo': "Anfield",
+        'goles': 3
     },
     {
-        nombre: "M. Nicolás",
-        equipo: "Liverfull",
-        goles: 3
+        'nombre': "M. Nicolás",
+        'equipo': "Liverfull",
+        'goles': 3
     }
 ]
 
@@ -47,27 +47,22 @@ function mediaGoles(){
 
 function mostrarTabla(){
     let contentTabla = "";
-    
+    let lastRow = '<tr class = "filaAdd"> <td> <input type="text" name="" class = "inAddName" id="IdInputNombre" placeholder="Nombre" required> </td>' + 
+    '<td> <input type="text" name="" id="IdInputEquipo" class = "inAddTeam" placeholder="Equipo" required> </td>' +
+    '<td> <input type="text" name="" id="IdInputGoles" class = "inAddGoal" placeholder="Goles" required> </td>' +
+    '<td> <input type="button" value="Add" id="idBtnAdd" class = "inAddBtn" > </td> </tr>';
     for (let index = 0; index < rowsGoles.length; index++) {
         if((index%2) == 0){
-            contentTabla = contentTabla + "<tr> <td>" + rowsGoles[index].nombre + "</td>" + 
-                                                "<td>" + rowsGoles[index].equipo + "</td>" +
-                                                "<td>" + rowsGoles[index].goles + "</td>" + 
-                                                '<td><button class = '+ '"btnDel"> Del </button> </td></tr>';
+            contentTabla = contentTabla + "<tr> <td>" + rowsGoles[index].nombre + "</td>" ;
         }else{
-            contentTabla = contentTabla + '<tr class= "distinctRow" > <td>' + rowsGoles[index].nombre + "</td>" +
-                                                "<td>" + rowsGoles[index].equipo + "</td>" +
-                                                "<td>" + rowsGoles[index].goles + "</td>" +
-                                                '<td><button class = '+ '"btnDel"> Del </button> </td></tr>';
-
+            contentTabla = contentTabla + '<tr class= "distinctRow" > <td>' + rowsGoles[index].nombre +
+             "</td>";
         }
+        contentTabla += "<td>" + rowsGoles[index].equipo + "</td>" + "<td>" + rowsGoles[index].goles +
+         "</td>" + '<td><button class = '+ '"btnDel"> Del </button> </td></tr>'
     }
-    contentTabla += '<tr class = "filaAdd"><td><input type="text" name="" class = "inAddName" id="IdInputNombre" placeholder="Nombre" required></td>' + 
-    '<td><input type="text" name="" id="IdInputEquipo" class = "inAddTeam" placeholder="Equipo" required></td>' +
-    '<td><input type="text" name="" id="IdInputGoles" class = "inAddGoal" placeholder="Goles" required></td>' +
-    '<td><input type="button" value="Add" id="idBtnAdd" class = "inAddBtn" ></td> </tr>';
-    
-    document.querySelector("#idTBodyGoles").innerHTML = contentTabla;
+    document.querySelector("#idTBodyGoles").innerHTML = contentTabla + lastRow;
+
     document.querySelector("#idBtnAdd").addEventListener("click", addRow);
     let botones = document.querySelectorAll(".btnDel");
     for (let index = 0; index < botones.length; index++) {
